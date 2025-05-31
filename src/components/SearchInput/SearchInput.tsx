417aa9a28bbcf4d633d6f9105b050ae5c2c79f5d
Yevhenii Orhanistyi <organistiy@gmail.com>
2025-05-31 11:42:56 +0200

    feat: properly clear debounce timeout on unmount and update state inside useEffect

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -27,7 +27,7 @@ const SearchInput = ({ searchParams, setSearchParams }: ICommonCatalogProps) => 
 
     return () => clearTimeout(delayInputTimeoutId);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [debouncedInputValue, 500]);
+  }, [debouncedInputValue, isTyping]);
 
   return (
     <div className="relative w-full">
