63355d4596820937336024723574a0325b3c28fb
Yevhenii Orhanistyi <organistiy@gmail.com>
2025-05-31 09:13:50 +0200

    feat: define constants for pagination, sorting, filters (brands, sizes, colors)

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -38,12 +38,66 @@ export const SOCIAL_LOGOS = [
   'twitter.png'
 ];
 
+export const ITEMS_PER_PAGE = 6;
+
 export const INITIAL_SEARCH_PARAMS: ISearchParams = {
   offset: 0,
+  limit: ITEMS_PER_PAGE,
   term: '',
-  sortValue: 'price asc',
+  sort: 'price asc',
   colors: [],
   sizes: [],
   brands: [],
   prices: [0, 1000]
 };
+
+export const SORTING_OPTIONS = [
+  { label: 'Name: A - Z', value: 'name.en-us asc' },
+  { label: 'Name: Z - A', value: 'name.en-us desc' },
+  { label: 'Price: Low - High', value: 'price asc' },
+  { label: 'Price: High - Low', value: 'price desc' }
+];
+
+export const BRANDS = [
+  { label: 'Addidas', value: 'Addidas' },
+  { label: 'Nike', value: 'Nike' },
+  { label: 'ECCO', value: 'ECCO' },
+  { label: 'Fila', value: 'Fila' },
+  { label: 'Puma', value: 'Puma' },
+  { label: 'Reebok', value: 'Reebok' },
+  { label: 'Lacoste', value: 'Lacoste' },
+  { label: 'New Balance', value: 'New Balance' },
+  { label: 'Ellesse', value: 'Ellesse' },
+  { label: 'Converse', value: 'Converse' },
+  { label: 'Asics', value: 'Asics' }
+];
+
+export const SIZES = [
+  { label: '33', value: '33' },
+  { label: '34', value: '34' },
+  { label: '35', value: '35' },
+  { label: '36', value: '36' },
+  { label: '37', value: '37' },
+  { label: '38', value: '38' },
+  { label: '39', value: '39' },
+  { label: '40', value: '40' },
+  { label: '41', value: '41' },
+  { label: '42', value: '42' },
+  { label: '43', value: '43' },
+  { label: '44', value: '44' },
+  { label: '45', value: '45' },
+  { label: '46', value: '46' }
+];
+
+export const COLORS = [
+  { label: 'Black', value: 'Black' },
+  { label: 'Red', value: 'Red' },
+  { label: 'Blue', value: 'Blue' },
+  { label: 'Green', value: 'Green' },
+  { label: 'Pink', value: 'Pink' },
+  { label: 'White', value: 'White' },
+  { label: 'Gray', value: 'Gray' },
+  { label: 'Yellow', value: 'Yellow' }
+];
+
+export const LANGUAGE_CODE = 'en-US';
