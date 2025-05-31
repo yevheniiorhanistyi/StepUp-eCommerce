4a984b0b6e423d7b9de42f42ed03ac8522ae7d78
Yevhenii Orhanistyi <organistiy@gmail.com>
2025-05-31 09:18:21 +0200

    feat: add global interfaces for search, categories, and products

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,5 @@
+import { ProductProjection, Category } from '@commercetools/platform-sdk';
+
 export interface IAuthContextType {
   isAuthenticated: boolean;
   setAuthentication: React.Dispatch<React.SetStateAction<boolean>>;
@@ -12,10 +14,63 @@ export interface IAuthStatus {
 
 export interface ISearchParams {
   offset: number;
+  limit: number;
   term: string;
-  sortValue: string;
+  sort: string;
   colors: string[];
   sizes: string[];
   brands: string[];
   prices: [number, number];
+}
+
+export interface ICategoryItem {
+  label: string;
+  value: string;
+}
+
+export interface ICategoryMenuProps {
+  categorySlug: string;
+  onCategoryClick: (slug: string) => void;
+}
+
+export interface ICategoryNode extends Category {
+  children: ICategoryNode[];
+}
+
+export interface ISearchParamsArrayTypes {
+  colors: string;
+  sizes: string;
+  brands: string;
+}
+
+export interface IProductItemParams {
+  product: ProductProjection;
+}
+
+export interface IProductListParams {
+  products: ProductProjection[];
+  isLoading: boolean;
+}
+
+export interface ICommonCatalogProps {
+  searchParams: ISearchParams;
+  setSearchParams: React.Dispatch<React.SetStateAction<ISearchParams>>;
+}
+
+export interface ISidebarFilterGroupProps extends ICommonCatalogProps {
+  label: string;
+  labelList: ICategoryItem[];
+  propertyToChange: keyof ISearchParamsArrayTypes;
+}
+
+export interface ICatalogSidebarProps extends ICommonCatalogProps {
+  categorySlug: string;
+  onCategoryClick: (slug: string) => void;
+}
+
+export interface IAppPaginationProps {
+  totalItems: number;
+  itemsPerPage: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
 }
