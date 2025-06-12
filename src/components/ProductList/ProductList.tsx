13433d807a68f9804724148227516ec09ee4c879
Yevhenii Orhanistyi <organistiy@gmail.com>
2025-06-12 12:19:04 +0200

    feat: integrate useCart in ProductList to pass cart data to products

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,7 +1,20 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { LineItem } from '@commercetools/platform-sdk';
 import { IProductListParams } from '@/types/types';
+import { useCart } from '@/context/CartContext';
+
 import ProductItem from '@/components/ProductItem/ProductItem';
 
 const ProductList = ({ products, isLoading }: IProductListParams): JSX.Element => {
+  const [cartItems, setCartItems] = useState<LineItem[]>([]);
+  const { cart, addItem, removeItem } = useCart();
+
+  useEffect(() => {
+    if (cart) setCartItems(cart.lineItems);
+  }, [cart]);
+
   if (isLoading) {
     return (
       <div className="flex justify-center items-center w-full h-full">
@@ -13,7 +26,13 @@ const ProductList = ({ products, isLoading }: IProductListParams): JSX.Element =
   return (
     <div className="grid w-full mb-10 gap-5 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
       {products.map((product) => (
-        <ProductItem key={product.id} product={product} />
+        <ProductItem
+          key={product.id}
+          product={product}
+          cartItems={cartItems}
+          addItem={addItem}
+          removeItem={removeItem}
+        />
       ))}
     </div>
   );
