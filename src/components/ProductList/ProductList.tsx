22f2f4deb78c98bd77b0596013e029a2208643e9
Yevhenii Orhanistyi <organistiy@gmail.com>
2025-06-13 11:25:49 +0200

    refactor: remove cart logic from ProductList and delegate to ProductItem

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,20 +1,8 @@
-'use client';
-
-import { useEffect, useState } from 'react';
-import { LineItem } from '@commercetools/platform-sdk';
 import { IProductListParams } from '@/types/types';
-import { useCart } from '@/context/CartContext';
 
 import ProductItem from '@/components/ProductItem/ProductItem';
 
 const ProductList = ({ products, isLoading }: IProductListParams): JSX.Element => {
-  const [cartItems, setCartItems] = useState<LineItem[]>([]);
-  const { cart, addItem, removeItem } = useCart();
-
-  useEffect(() => {
-    if (cart) setCartItems(cart.lineItems);
-  }, [cart]);
-
   if (isLoading) {
     return (
       <div className="flex justify-center items-center w-full h-full">
@@ -26,13 +14,7 @@ const ProductList = ({ products, isLoading }: IProductListParams): JSX.Element =
   return (
     <div className="grid w-full mb-10 gap-5 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
       {products.map((product) => (
-        <ProductItem
-          key={product.id}
-          product={product}
-          cartItems={cartItems}
-          addItem={addItem}
-          removeItem={removeItem}
-        />
+        <ProductItem key={product.id} product={product} />
       ))}
     </div>
   );
