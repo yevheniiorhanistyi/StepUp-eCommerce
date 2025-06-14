2d9ba0fe68b187159f30f48fbfa1e2949e9824d2
Dzmitry Drevich <jeidan2012@gmail.com>
2025-06-15 00:46:41 +0200

    feat: add order summary block

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import CartList from '@/components/Cart/CartList';
+import OrderBlock from '@/components/Cart/OrderBlock';
 import SpinnerFallback from '@/components/SpinnerFallback/SpinnerFallback';
 import { useCart } from '@/context/CartContext';
 
@@ -12,6 +13,7 @@ const Cart = (): JSX.Element => {
   return (
     <div className="flex justify-between min-[767.97px]:flex-row flex-col px-5 sm:px-10 py-10 gap-5 w-full max-w-[1520px] mx-auto relative">
       <CartList />
+      <OrderBlock />
     </div>
   );
 };
