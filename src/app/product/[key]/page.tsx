aa4e48f233f0f3a0e154e7ef540bb17d248059be
Yevhenii Orhanistyi <organistiy@gmail.com>
2025-06-15 10:07:14 +0200

    fix: remove duplicate component declaration

--- src/app/product/[key]/page.tsx
+++ src/app/product/[key]/page.tsx
@@ -7,7 +7,6 @@ import {
   AccordionTrigger
 } from '@/components/ui/accordion';
 import { ProductSlider } from '@/components/ProductSlider/ProductSlider';
-import PriceDisplay from '@/components/PriceDisplay/PriceDisplay';
 
 import ProductSizePicker from '@/components/ProductSizePicker/ProductSizePicker';
 import PriceDisplay from '@/components/PriceDisplay/PriceDisplay';
